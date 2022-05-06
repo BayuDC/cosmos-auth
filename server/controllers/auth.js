3dd9cd2f6a0faae549bc817e8fcbe92905a4d35f
BayuDC <dev.bayudc@gmail.com>
2022-05-06 10:31:32 +0700

    server: add user login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,15 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { secret } = require('../config');
+
+const generateToken = user => {
+    const payload = { user: { id: user.id, name: user.name, email: user.email } };
+    const options = { expiresIn: 24 * 60 * 60 };
+
+    return jwt.sign(payload, secret, options);
+};
 
 /**
  *
@@ -23,7 +34,18 @@ module.exports.signup = async (req, res) => {
  * @param {import('express').Request} req
  * @param {import('express').Response} res
  */
-module.exports.login = (req, res) => {};
+module.exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    const auth = await bcrypt.compare(password || '', user.password);
+    if (!auth) return res.status(401).json({ error: 'Password incorrect' });
+
+    const token = generateToken(user);
+    res.status(201).json({ token });
+};
 /**
  *
  * @param {import('express').Request} req
