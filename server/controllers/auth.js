ad918f707e159f179cc50a1034a6b40c0e9e3798
BayuDC <dev.bayudc@gmail.com>
2022-05-06 09:57:36 +0700

    server: define auth routes and controller

---
+++ server/controllers/auth.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+module.exports.status = (req, res) => {};
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+module.exports.register = (req, res) => {};
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+module.exports.login = (req, res) => {};
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+module.exports.me = (req, res) => {};
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
+const authController = require('../controllers/auth');
 
 router.get('/', (req, res) => {
     res.json({
         message: 'Hello world',
     });
 });
+
+router.get('/auth', authController.status);
+router.post('/auth/login', authController.login);
+router.post('/auth/register', authController.register);
+router.get('/auth/me', authController.me);
 
 module.exports = router;
