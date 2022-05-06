71fc43ffb699ddda6d13011a7744bcd3072eeaab
BayuDC <dev.bayudc@gmail.com>
2022-05-06 14:57:36 +0700

    client: fix typo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,10 +2,10 @@ import github from '../assets/github-logo.png';
 
 function Home() {
     return (
-        <section class="hero is-warning">
-            <div class="hero-body">
-                <p class="title">Welcome to CosmosAuth</p>
-                <p class="subtitle">A simple implementation of jwt auth in mern stack</p>
+        <section className="hero is-warning">
+            <div className="hero-body">
+                <p className="title">Welcome to CosmosAuth</p>
+                <p className="subtitle">A simple implementation of jwt auth in mern stack</p>
                 <a href="https://github.com/BayuDC" target="_blank" className="button">
                     <span className="icon">
                         <img src={github} alt="github logo" />
