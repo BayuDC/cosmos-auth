6b4bc841dc04406005871e2e8debd012ea509df7
BayuDC <dev.bayudc@gmail.com>
2022-05-06 09:36:25 +0700

    server: express and mongo setup

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const { port, mongouri } = require('./config');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', require('./routes/api'));
+app.use('/', require('./routes/web'));
+
+mongoose.connect(mongouri).then(() => {
+    app.listen(port, () => {
+        console.log('Running at port', port);
+    });
+});
---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    port: 8080,
+    mongouri: 'mongodb://bayudc:bay@127.0.0.1:27017/cosmos-db?authSource=admin',
+    secret: 'wangy12345',
+};
